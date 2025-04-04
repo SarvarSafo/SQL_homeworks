8548685346085a2d9f168a092fb0d5f3789d25c9
Sarvarjon <88224448+SarvarSafo@users.noreply.github.com>
2025-04-04 19:08:48 +0500

    Update hw.sql

--- lesson-1/homework/hw.sql
+++ lesson-1/homework/hw.sql
@@ -1,10 +1,19 @@
+-- Easy
 -- 1. Define the following terms: data, database, relational database, and table.
+  -- Answer:
+  -- Data: Raw facts, numbers, text, or symbols that represent information.
+  -- Database: A structured collection of data stored electronically.
+  -- Relational Database: Relational Database (RDBMS): A type of database that organizes data into tables with rows and columns, linked by relationships. 
+  -- Table: A structure within a relational database that stores data in rows (records) and columns (fields).
 
-ehdkjjj
-qwwlkdjk
-dwjjnqjhbnkj
-2.
+-- 2. List five key features of SQL Server.
+  -- Answer:
+  -- Data Security, HADR, Performance Optimization, Comprehensive Data Management, Analytics
 
-3
-4
+-- 3. What are the different authentication modes available when connecting to SQL Server? (Give at least 2)
+  -- Answer:
+  -- We have Windows Authenfication,SQL Server Authenfication and Microsoft Entra(several) 
+
+-- Medium
+-- 
 
